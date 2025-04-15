a8d21d081b371a0ec86dbb0723519e5d5862d85f
popiionut04 <ionut.popi04@e-uvt.ro>
2025-04-15 05:15:08 +0300

    Update file.c

--- file.c
+++ file.c
@@ -1,15 +1,9 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define AC_BLACK "\x1b[30m"
-#define AC_RED "\x1b[31m"
 #define AC_GREEN "\x1b[32m"
 #define AC_YELLOW "\x1b[33m"
 #define AC_BLUE "\x1b[34m"
-#define AC_MAGENTA "\x1b[35m"
-#define AC_CYAN "\x1b[36m"
-#define AC_WHITE "\x1b[37m"
 #define AC_NORMAL "\x1b[m"
 typedef struct Task
 {
